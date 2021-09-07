7478dadd41886158049e0ca18a899b8b6fadd8b5
YuliyaCher <y.chernikava@gmail.com>
2021-09-07 10:58:12 +0300

    fix lesson-4

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 // Напишите функцию sum, которая возвращает сумму чисел
 function sum(a) {
-return function (b) {
+  return function (b) {
     return a + b;
-};
+  };
 }
 console.log(sum(2)(3));
-
-
 
 // Покрасьте абзацы по клику
 const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
@@ -14,18 +12,17 @@ const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
 const p = document.querySelectorAll('p');
 
 function changeColor() {
-let i = 0;
-return function (e) {
+  let i = 0;
+  return function (e) {
     e.target.style.color = colors[i];
     i++;
     if (i >= colors.length) {
-    i = 0;
+      i = 0;
     }
-};
-};
+  };
+}
 
 p.forEach((item) => {
-const color = changeColor();
-item.addEventListener('click', color);
+  const color = changeColor();
+  item.addEventListener('click', color);
 });
-
