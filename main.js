d3a521bd00052796e69da4a59381c3cb0800b4bf
YuliyaCher <y.chernikava@gmail.com>
2021-11-02 20:01:03 +0300

    fix lesson-10

--- main.js
+++ main.js
@@ -22,7 +22,9 @@ const data = [
     city: 'Berlin',
     country: 'Germany',
     imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379364/fe/hostel-friendship_aw6tn7.jpg',
-  },
+  }
+];
+const data2 = [
   {
     name: 'Radisson Blu Hotel',
     city: 'Kyiv',
@@ -46,21 +48,47 @@ const data = [
     city: 'Port Dickson',
     country: 'Malaysia',
     imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/the-andaman-resort_d2xksj.jpg',
-  },
+  }
 ];
 
 const hotelsList = document.querySelector('.gallery');
+data.forEach(el => {
+  const li = document.createElement('li');
+  li.classList.add('gallery__item');
+  li.innerHTML = `
+  <img src=${el.imageUrl} alt='photo'/>
+  <h3><a href=#>${el.name}</a></h3>
+  <p>${el.country} , ${el.city}</p>
+  `;
+  hotelsList.append(li);
+});
 
-const createHotelsList = arr => {
-  arr.forEach(el => {
-    const li = document.createElement('li');
-    li.classList.add('gallery__item');
-    li.innerHTML = `
-    <img src=${el.imageUrl} alt='photo'/>
-    <h3><a href=#>${el.name}</a></h3>
-    <p>${el.city}, ${el.country}</p>
-    `;
-    hotelsList.append(li);
-  });
-};
-createHotelsList(data);
+const hotelsList2 = document.querySelector('.gallery');
+data2.forEach(el => {
+  const li = document.createElement('li');
+  li.classList.add('gallery__item');
+  li.innerHTML = `
+  <img src=${el.imageUrl} alt='photo'/>
+  <h3><a href=#>${el.name}</a></h3>
+  <p>${el.country} , ${el.city}</p>
+  `;
+  hotelsList2.append(li);
+});
+
+// вариант 2 (что был до этого)////////////////////
+
+// const hotelsList = document.querySelector('.gallery');
+
+// const createHotelsList = arr => {
+//   arr.forEach(el => {
+//     const li = document.createElement('li');
+//     li.classList.add('gallery__item');
+//     li.innerHTML = `
+//     <img src=${el.imageUrl} alt='photo'/>
+//     <h3><a href=#>${el.name}</a></h3>
+//     <p>${el.city}, ${el.country}</p>
+//     `;
+//       hotelsList.append(li);
+//   });
+// };
+// createHotelsList(data);
